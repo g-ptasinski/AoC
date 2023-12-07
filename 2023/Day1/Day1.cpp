9704710e81808f2e0cef707a0f6915cb3ddb12c2
grzesioo66 <grzesioo66@gmail.com>
2023-12-07 22:07:45 +0100

    Day1 part 2 to be done

--- 2023/Day1/Day1.cpp
+++ 2023/Day1/Day1.cpp
@@ -3,13 +3,14 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <fstream>
-#include "Day1.h"
 #include <algorithm>
+#include "Day1.h"
+#include <map>
 
 int main ()
 {
-  part1();
-  //part2();
+  //part1();
+  part2();
   return 0;
 }
 
@@ -50,8 +51,26 @@ void part2()
 
   myfile.open ("input.txt");
 
+  ones = 0;
+  tens = 0;
+
+  int iTensString = 0;
+  int iOnesString = 0;
+   
+  std::map<std::string, int> Nums = {};
+  
   while (std::getline(myfile, line))
   {
+    for(auto &element : Numbers)
+    {
+      std::size_t found = line.find(element);
+      if (found!=std::string::npos)
+        {
+           Nums.insert ( std::pair<std::string ,int>(element,found) );
+        }
+    }
+    
+
     for (std::string::size_type i = 0; i < line.size(); i++)
     {
       if(isdigit(line[i]))
@@ -59,7 +78,6 @@ void part2()
           tens = line[i]-48;
           break;
         }
-      else if()
     }
     for (std::string::size_type i = line.size(); i >= 0 ; i--)
     {
@@ -69,12 +87,24 @@ void part2()
           break;
         }
     }
+/*
+    auto min = min_element(Nums.begin(), Nums.end(),
+                      [](decltype(Nums)::value_type& l, decltype(Nums)::value_type& r) -> bool { return l.second < r.second; });
+
+    auto max = max_element(Nums.begin(), Nums.end(),
+                      [](decltype(Nums)::value_type& l, decltype(Nums)::value_type& r) -> bool { return l.second > r.second; });
+
+    std::cout<<min.first<<" "<<min.second;
+*/
+    (tens>iTensString) ? (tens=tens) : (tens=iTensString);
+    (tens>iTensString) ? (tens=tens) : (tens=iTensString);
 
     sum+=10*tens+ones;
   }
   std::cout<<sum<<std::endl;
   myfile.close();
 }
+
 /*
 zero
 one
@@ -83,6 +113,6 @@ three
 four
 six
 seven
-eigh
+eight
 nine
 */
--- 2023/Day1/Day1.h
+++ 2023/Day1/Day1.h
@@ -1,10 +1,12 @@
 void part1();
 void part2();
-bool 
 
 std::string line;
 std::string ch;
-
+  
+std::string Numbers[10] =
+   {"zero","one","two","three","four","six","seven","eight","nine"};
+   
 int sum = 0;
 int tens= 0;
 int ones= 0;
